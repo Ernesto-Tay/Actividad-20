e508e02f5874f40cfaa453be2aa8bcf1447c7657
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-08-20 19:08:37 -0600

    Creación de clase Consola: clase con métodos para obtener datos de las clases anteriores (práctica de obtención de datos)

--- main.py
+++ main.py
@@ -53,3 +53,49 @@ class Empresa:
         return self._deptos
     def updateDeptos(self,deptos_list):
         self._deptos = deptos_list
+
+empresa = Empresa("123","wasa")
+class ConsolaPrints:
+    def mostrarDeptos(empresa):
+        deptos_list = empresa.getDeptos()
+        if not deptos_list:
+            print("No hay departamentos asignados aún")
+        else:
+            print("\n" + "-"*10 + " LISTA DE DEPARTAMENTOS " + "-"*10)
+            print("ID".ljust(20) + "NOMBRE".ljust(35) + "NO. EMPLEADOS".ljust(20))
+            for depto in deptos_list:
+                print(str(depto.getID()).ljust(20) +str(depto.getNombre()).ljust(35) + str(len(depto.getEmpleados())).ljust(20))
+
+    def obtenerDepto(empresa,nombre_depto):
+        deptos_list = empresa.getDeptos()
+        if not deptos_list:
+            print("No hay departamentos asignados aún")
+        else:
+            for depto in deptos_list:
+                if depto.getNombre() == nombre_depto:
+                    return depto
+            if not any(nombre_depto == depto.getNombre() for depto in deptos_list):
+                return False
+
+    def mostrarEmpleados(depto):
+        empleados_list = depto.getEmpleados()
+        if not empleados_list:
+            print("No hay empleados en el departamento")
+        else:
+            print("\n" + "-" * 10 + " LISTA DE EMPLEADOS " + "-" * 10)
+            print("ID".ljust(20) + "NOMBRE".ljust(35) + "PUESTO".ljust(20)+ "SALARIO".ljust(20))
+            for empleado in empleados_list:
+                print(str(empleado.getID()).ljust(20) + str(empleado.getNombre()).ljust(35) + str(empleado.getPuesto()).ljust(20)+ f"Q{empleado.getSalario()}".ljust(20))
+
+    def obtenerEmpleado(depto, nombre_empleado):
+        empleados_list = depto.getEmpleados()
+        if not empleados_list:
+            print("No hay empleados en el departamento")
+        else:
+            for empleado in empleados_list:
+                if empleado.getNombre() == nombre_empleado:
+                    return empleado
+            if not any(nombre_empleado == empleado.getNombre() for empleado in empleados_list):
+                return False
+
+
