e0c9bdc90aadb76022397730f9471636a9de2224
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-08-20 18:24:24 -0600

    Creación de métodos para obtener copias de datos inalterables

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ class Empleado:
         self.__puesto = puesto
         self._salario = salario
 
+    def getID(self):
+        return self.__ID
+    def getNombre(self):
+        return self.__nombre
+    def getPuesto(self):
+        return self.__puesto
+
+
 # ID y Nombre son privados para evitar cambios, pero la lista "empleados" es restringida para permitir cambios controlados
 class Departamento:
     def __init__(self,ID,nombre):
@@ -13,9 +21,20 @@ class Departamento:
         self.__nombre = nombre
         self._empleados = []
 
+    def getID(self):
+        return self.__ID
+    def getNombre(self):
+        return self.__nombre
+
+
 # Lo mismo que en la clase "Departamento"
 class Empresa:
     def __init__(self,ID,nombre):
         self.__ID = ID
         self.__nombre = nombre
         self._deptos = []
+
+    def getID(self):
+        return self.__ID
+    def getNombre(self):
+        return self.__nombre
